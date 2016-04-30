e211ed48a39b2583cc83131c9d0249eddc4a4658
Dakota Kronberger <kodykronberger@yahoo.com>
2016-04-30 18:04:27 -0500

    Initial commit.

---
+++ WordTree/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WordTree
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// The parent node which contains all sub-nodes.
+        /// </summary>
+        private Node firstNode;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Main();
+        }
+
+        /// <summary>
+        /// Initialization function.
+        /// </summary>
+        private void Main() 
+        {
+            // Create starting node
+            this.firstNode = new Node("Tree");
+        }
+
+        /// <summary>
+        /// Renders the TreeView.
+        /// </summary>
+        private void Render()
+        {
+            this.wordTreeView.Items.Clear();
+            this.wordTreeView.Items.Add(this.CreateTreeViewItem(firstNode));
+        }
+
+        /// <summary>
+        /// Creates a TreeViewItem recursively for each item in the node tree.
+        /// </summary>
+        /// <param name="currentNode">The current node to create an item from</param>
+        /// <returns>The TreeViewItem created</returns>
+        private TreeViewItem CreateTreeViewItem(Node currentNode)
+        {
+            TreeViewItem result = new TreeViewItem
+            {
+                Header = currentNode.Value
+            };
+
+            foreach (Node node in currentNode.Children)
+            {
+                result.Items.Add(this.CreateTreeViewItem(node));
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Adds a word to the word tree.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void addWordButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.firstNode = Node.AddWord(this.firstNode, this.addWordTextBox.Text);
+
+            // Render
+            this.Render();
+        }
+
+        /// <summary>
+        /// Checks to see if all characters in the text box are alpha.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void addWordTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(Regex.IsMatch(this.addWordTextBox.Text, @"^[a-zA-Z]+$"))
+            {
+                this.addWordButton.IsEnabled = true;
+            }
+            else
+            {
+                this.addWordButton.IsEnabled = false;
+            }
+        }
+    }
+}
---
+++ WordTree/Node.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace WordTree
+{
+    public class Node
+    {
+        public string Value { get; set; }
+        public List<Node> Children { get; set; }
+        public Node()
+        {
+            this.Children = new List<Node>();
+        }
+        public Node(string value)
+            : this()
+        {
+            this.Value = value;
+        }
+
+        public static Node AddWord(Node firstNode, string word)
+        {
+            Node currentNode = firstNode;
+            Node nextNode;
+
+            foreach (char c in word.ToLower())
+            {
+                // Get char value
+                string value = c.ToString();
+
+                // Find a node that contains the current value
+                Node foundValue = currentNode.Children.FirstOrDefault(n => n.Value == value);
+
+                // If node was found, no need to add duplicate nodes. 
+                // Else, add a new node with the char value
+                if (foundValue != null)
+                {
+                    currentNode = foundValue;
+                }
+                else
+                {
+                    nextNode = new Node(c.ToString());
+                    currentNode.Children.Add(nextNode);
+                    currentNode = nextNode;
+                }
+            }
+
+            // Return parent node
+            return firstNode;
+        }
+    }
+}
